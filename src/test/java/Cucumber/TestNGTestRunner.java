1ccfd1228c985a87ec9b9cecf73cf459cd01d828
kevin_gerardo <kevingerardo23@gmail.com>
2025-03-24 19:12:21 -0500

    add cucumber logic

--- src/test/java/Cucumber/TestNGTestRunner.java
+++ src/test/java/Cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/Cucumber",
-        glue = "StepDefinitions",
+        glue = "StepsDefinitions",
         monochrome = true,
         plugin = {"html:reports/cucumber.html"},
         tags = "@ErrorValidation")
---
+++ src/test/java/StepsDefinitions/CucumberHooks.java
@@ -0,0 +1,12 @@
+package StepsDefinitions;
+
+import TestComponents.BaseTest;
+import io.cucumber.java.After;
+
+public class CucumberHooks extends BaseTest {
+
+    @After
+    public void close_driver(){
+        tearDown();
+    }
+}
